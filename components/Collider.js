e42ddcae4cceb8911209c7cd7b9ac3fab41f429f
Sanchita210507 <72611487+Sanchita210507@users.noreply.github.com>
2022-04-12 20:55:20 +0530

    Add files via upload

---
+++ components/Collider.js
@@ -0,0 +1,26 @@
+AFRAME.registerComponent("flying-birds", {
+init: function(){
+    for(var i = 1; i<=20; i++){
+        var bid = `bird${i}`
+
+        var posX =(Math.random() * 3000 + (-1000)); 
+        var posY = (Math.random() * 2 + (-1));
+        var posZ = (Math.random() * 3000 + (-1000));
+
+        var birdPosition = {x:posX, y:posY, z:posZ}
+        this.flyingBirds(bid, birdPosition)
+    }
+},
+
+flyingBirds:(id, position)=>{
+     var terrainEl = document.querySelector("#terrain")
+    var birdEl = document.createElement("a-entity")
+    birdEl.setAttribute("id",id)
+    birdEl.setAttribute("position",position)
+    birdEl.setAttribute("gltf-model","./assets/models/flying_bird/scene.gltf")
+    birdEl.setAttribute("animation-mixer", {})
+    birdEl.setAttribute("scale",{x:500, y: 500, z:500})
+    terrainEl.appendChild(birdEl)
+    
+}
+})
